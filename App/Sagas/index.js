b4ddd3fe13938eb070282a02a7d71ce094b75a7b
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 01:43:10 +0800

    remove fixtures code

--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -1,13 +1,12 @@
 import { all } from 'redux-saga/effects'
 import DebugConfig from '../Config/DebugConfig'
 import API from '../Services/Api'
-import FixtureAPI from '../Services/FixtureApi'
 
 /* ------------- API ------------- */
 
 // The API we use is only used from Sagas, so we create it here and pass along
 // to the sagas which need it.
-const api = DebugConfig.useFixtures ? FixtureAPI : API.create()
+const api = API.create()
 
 /* ------------- Connect Types To Sagas ------------- */
 
