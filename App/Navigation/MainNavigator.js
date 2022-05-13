eb2f7b6b91f204dab1f59b6eda08a161dee93e68
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 03:00:55 +0800

    add navigation and screen for Consent Form

--- App/Navigation/MainNavigator.js
+++ App/Navigation/MainNavigator.js
@@ -1,41 +1,46 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
+import ConsentFormScreen from '../Containers/ConsentFormScreen'
+import ConsentsScreen from '../Containers/ConsentsScreen'
 import HomeScreen from '../Containers/HomeScreen'
 import Colors from '../Themes/Colors'
 
+const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
-function ConsentScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Consent Screen</Text>
-    </View>
-  )
-}
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarStyle: {
+        // backgroundColor: Colors.background,
+        borderTopWidth: 1
+      },
+      tabBarLabelStyle: {
+        fontSize: 20,
+        textAlignVertical: 'center',
+        alignContent: 'center'
+      },
+      tabBarLabelPosition: 'beside-icon',
+      tabBarIconStyle: { display: 'none' },
+      headerTintColor: '#fff',
+      tabBarActiveTintColor: Colors.primary,
+      tabBarInactiveTintColor: 'gray'
+    })}>
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Consents" component={ConsentsScreen} />
+  </Tab.Navigator>
+)
 
 const MainNavigator = ({ navigation }) => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarStyle: {
-          // backgroundColor: Colors.background,
-          borderTopWidth: 1
-        },
-        tabBarLabelStyle: {
-          fontSize: 20,
-          textAlignVertical: 'center',
-          alignContent: 'center'
-        },
-        tabBarLabelPosition: 'beside-icon',
-        tabBarIconStyle: { display: 'none' },
-        headerTintColor: '#fff',
-        tabBarActiveTintColor: Colors.primary,
-        tabBarInactiveTintColor: 'gray'
-      })}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Consent" component={ConsentScreen} />
-    </Tab.Navigator>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Home">
+      <Stack.Screen name="Home" component={TabNavigator} />
+      <Stack.Screen name="Consent" component={ConsentFormScreen} />
+    </Stack.Navigator>
   )
 }
 
