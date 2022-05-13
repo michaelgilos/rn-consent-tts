4a693d613d46ac5134fa4c7e3ea5b65d14dd1727
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 04:14:48 +0800

    working implementation to play tts for consent

--- App/Containers/ConsentFormScreen.js
+++ App/Containers/ConsentFormScreen.js
@@ -3,13 +3,14 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { connect } from 'react-redux'
 import Images from '../Themes/Images'
 import i18n from 'i18n-js'
+import Tts from 'react-native-tts'
 
 i18n.fallbacks = true
 
 i18n.translations = {
   en: {
     consent:
-      'You understand that by using the site or site services, you agree to be bound by this agreement.If you do not accept this agreement in its entirety, you must not access or use the site or the site services.\n\nDo you agree to this agreement?\nPlease respond by saying "Yes" or "No".'
+      'You understand that by using the site or site services, you agree to be bound by this agreement. If you do not accept this agreement in its entirety, you must not access or use the site or the site services.\n\nDo you agree to this agreement?\nPlease respond by saying "Yes" or "No".'
   },
   fr: {
     consent: `Vous comprenez qu'en utilisant le site ou les services du site, vous acceptez d'être lié par cet accord. Si vous n'acceptez pas cet accord dans son intégralité, vous ne devez pas accéder ou utiliser le site ou les services du site.\n\nAcceptez-vous à cet accord ?\nVeuillez répondre en disant "Oui" ou "Non".`
@@ -20,9 +21,33 @@ i18n.translations = {
 
 const ConsentsScreen = ({ navigation, route }) => {
   const { name, language } = route.params
+  
   i18n.locale = language
 
   const [ttsCompleted, setTtsCompleted] = useState(false)
+
+  useEffect(() => {
+    Tts.setDefaultLanguage(language)
+
+    Tts.engines().then((engines) => console.tron.log({ engines }))
+
+    Tts.addEventListener('tts-finish', (event) => {
+      setTtsCompleted(true)
+      console.tron.log({ event })
+    })
+
+    Tts.getInitStatus()
+      .then(() => {
+        Tts.speak(i18n.t('consent'))
+      })
+      .catch((e) => {
+        if (err.code === 'no_engine') {
+          Tts.requestInstallEngine()
+        }
+      })
+
+    return Tts.removeAllListeners('tts-finish')
+  }, [])
 
   return (
     <View style={styles.container}>
