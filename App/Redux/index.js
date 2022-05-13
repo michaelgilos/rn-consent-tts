e5dc4e9c4c30ac1605ea465d1d5219ee13daf151
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 01:43:23 +0800

    add missing import

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
 import ReduxPersist from '../Config/ReduxPersist'
 import rootSaga from '../Sagas/'
 import configureStore from './CreateStore'
