9957f5c48369839fd9afda0ae3677a185c3206c7
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 01:59:27 +0800

    use new api

--- App/Config/index.js
+++ App/Config/index.js
@@ -1,8 +1,8 @@
+import { LogBox } from 'react-native'
 import DebugConfig from './DebugConfig'
-import AppConfig from './AppConfig' // eslint-disable-line no-unused-vars
 
 if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
   // it off, but the healthier approach is to fix the warnings.  =)
-  console.disableYellowBox = !DebugConfig.yellowBox
+  LogBox.ignoreAllLogs(!DebugConfig.yellowBox)
 }
