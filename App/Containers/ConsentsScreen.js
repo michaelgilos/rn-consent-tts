cf15d63f9b7e242f8dad843c2d71b667be6eafaf
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 02:56:21 +0800

    add screen for consents

---
+++ App/Containers/ConsentsScreen.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { connect } from 'react-redux'
+
+const ConsentsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.consentText}>All Consents</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 40
+  },
+  consentText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  },
+  labelText: {
+    fontSize: 14
+  },
+  inputText: {
+    borderColor: '#000',
+    borderWidth: 1
+  }
+})
+
+export default connect(null, null)(ConsentsScreen)
