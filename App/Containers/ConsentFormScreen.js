bcd47f31953caf35968558566c9149e629851bc7
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 04:50:36 +0800

    update only on language change

--- App/Containers/ConsentFormScreen.js
+++ App/Containers/ConsentFormScreen.js
@@ -55,7 +55,7 @@ const ConsentsScreen = ({ saveConsent, route }) => {
       Tts.stop()
       Tts.removeAllListeners('tts-finish')
     }
-  }, [])
+  }, [language])
 
   const onRetry = () => {
     playConsent()
