998234378953f9ab703f7801d347e67c0f0f765e
Michael Gilos <gilosmichael@gmail.com>
2022-05-14 01:38:20 +0800

    remove unused code

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -5,9 +5,9 @@ import configureStore from './CreateStore'
 
 /* ------------- Assemble The Reducers ------------- */
 export const reducers = combineReducers({
-  nav: require('./NavigationRedux').reducer,
-  github: require('./GithubRedux').reducer,
-  search: require('./SearchRedux').reducer
+  // nav: require('./NavigationRedux').reducer,
+  // github: require('./GithubRedux').reducer,
+  // search: require('./SearchRedux').reducer
 })
 
 export default () => {
